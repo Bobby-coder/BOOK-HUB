4197c5eb1ef97f0888b89c39020c026044d23c9e
Bobby-coder <bobbysadhwani612@gmail.com>
2022-12-21 18:31:53 +0530

    Diffrent  Routes added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
+
 function App() {
   return (
-    <></>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
