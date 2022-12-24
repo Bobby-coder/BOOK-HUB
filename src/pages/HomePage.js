e42a546ed41151d291518031132cf0bea4230d59
Bobby-coder <bobbysadhwani612@gmail.com>
2022-12-24 23:13:06 +0530

    BookList component rendered

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import BookList from "../components/homepage/BookList"
+import HeroSection from "../components/homepage/HeroSection"
+
+const HomePage = () => {
+    return(
+        <>
+        <HeroSection />
+        <BookList />
+        </>
+    )
+}
+
+export default HomePage
