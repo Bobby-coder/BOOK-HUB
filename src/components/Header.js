73428f44a04b5dea3749654e4ab2b507674bc400
Bobby-coder <bobbysadhwani612@gmail.com>
2022-12-26 00:59:40 +0530

    dark mode added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import { NavLink } from "react-router-dom";
 import { BiSun, BiMoon } from "react-icons/bi";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
     <>
       <nav className="flex items-center justify-between h-[60px] bg-white border-b-2 border-solid dark:border-gray-700 px-4 lg:px-32 dark:bg-gray-900 fixed top-0 left-0 w-full z-20">
@@ -15,12 +30,24 @@ const Header = () => {
           </div>
         </NavLink>
         <div className="flex items-center">
-          <button>
-            <BiSun className="text-blue-600 dark:text-white h-6 w-6" />
+          <button onClick={handleTheme}>
+            <BiSun
+              className={
+                theme === "light"
+                  ? "text-blue-600 dark:text-white h-6 w-6"
+                  : "hidden"
+              }
+            />
           </button>
 
-          <button className="hidden">
-            <BiMoon className="text-blue-600 dark:text-white h-6 w-6" />
+          <button onClick={handleTheme}>
+            <BiMoon
+              className={
+                theme === "dark"
+                  ? "text-blue-600 dark:text-white h-5 w-5"
+                  : "hidden"
+              }
+            />
           </button>
         </div>
       </nav>
