84a40da2fd50c71685a9aa572431844c9c049908
Bobby-coder <bobbysadhwani612@gmail.com>
2022-12-25 19:31:41 +0530

    Single book page added

---
+++ src/pages/SingleBookPage.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import BookHeroSection from "../components/singlebookpage/BookHeroSection";
+
+const SingleBookPage = () => {
+  const [singleBookData, setSingleBookData] = useState({});
+  const { id } = useParams();
+  const fetchData = async () => {
+    const { data } = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes/${id}?key=AIzaSyAhGY-2G7H3XoXVn1szey9QjHpIQ9V60ro`
+    );
+    setSingleBookData(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <BookHeroSection
+        title={singleBookData.volumeInfo?.title}
+        description={singleBookData.volumeInfo?.description}
+        img={singleBookData.volumeInfo?.imageLinks?.small}
+        buyLink={singleBookData.saleInfo?.buyLink}
+      />
+    </>
+  );
+};
+
+export default SingleBookPage;
