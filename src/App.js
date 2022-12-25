9bdbe34f8abd66c6462c05da7cd9fabeb34e6c59
Bobby-coder <bobbysadhwani612@gmail.com>
2022-12-25 19:32:08 +0530

    single book page route added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ErrorPage from "./pages/ErrorPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SingleBookPage from "./pages/SingleBookPage";
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path='*' element={<ErrorPage />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/books/:id" element={<SingleBookPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
