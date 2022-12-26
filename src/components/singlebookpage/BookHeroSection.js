30f7897a29763a5454c5dbb54302b0fb8d11dfef
Bobby-coder <bobbysadhwani612@gmail.com>
2022-12-26 18:00:13 +0530

    Read More Modal bug fixed

--- src/components/singlebookpage/BookHeroSection.js
+++ src/components/singlebookpage/BookHeroSection.js
@@ -7,7 +7,7 @@ const BookHeroSection = ({ title, description, img, buyLink }) => {
   const [readMoreModal, setReadMoreModal] = useState(false);
   return (
     <>
-      <div className="bg-white dark:bg-gray-900 mt-[60px]">
+      <div className={readMoreModal ? "hidden" : "bg-white dark:bg-gray-900 mt-[60px]"}>
         <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
           <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
